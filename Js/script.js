fbc6599cc8aa45c260ff430181c877add9bd29eb
naheantarik <nahean.swe@gmail.com>
2022-09-28 01:25:11 +0600

    update

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,7 @@ let query = document.querySelector('.query');
 let serchBtn = document.querySelector('.searchBtn');
 
 
-serchBtn.onclick = function() {
-    let url ='https://www.google.com/search?q='+query.value;
-    window.open(url, '_self');
-}
+// serchBtn.onclick = function() {
+//     let url ='https://www.google.com/search?q='+query.value;
+//     window.open(url, '_self');
+// }
